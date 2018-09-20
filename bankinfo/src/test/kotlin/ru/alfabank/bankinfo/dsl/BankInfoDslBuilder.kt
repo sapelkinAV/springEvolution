effd2ee751f310e61ae58d56ec2132eab83763f3
Alexander <al.vi.sapelkin@gmail.com>
2018-09-20 16:43:04 +0300

    comment

--- bankinfo/src/test/kotlin/ru/alfabank/bankinfo/dsl/BankInfoDslBuilder.kt
+++ bankinfo/src/test/kotlin/ru/alfabank/bankinfo/dsl/BankInfoDslBuilder.kt
@@ -6,14 +6,19 @@ import ru.alfabank.bankinfo.model.BankBlzInfo
 class BankInfoDslBuilder{
     fun BlzInfoResponse(block: BankBlzInfo.() -> Unit): String {
         val bankInfo = BankBlzInfo().apply(block)
+        //В этой либе можно указывать xml неймспейсы, но я не сильно запаривался с этим
         return xml("soapenv:Envelope") {
             xmlns = "http://schemas.xmlsoap.org/soap/envelope/"
             "soapenv:Body"{
                 "ns1:getBankResponse"{
                     attribute("xmlns:ns1","http://thomas-bayer.com/blz/")
                     "ns1:details"{
-                        "ns1:bezeichnung"{ -bankInfo.bezeichnung }
-                        "ns1:bic"{ -bankInfo.bic }
+                        "ns1:bezeichnung"{
+                            -bankInfo.bezeichnung
+                        }
+                        "ns1:bic"{
+                            -bankInfo.bic
+                        }
                         "ns1:ort"{
                             -bankInfo.ort
                         }
