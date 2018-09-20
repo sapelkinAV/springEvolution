f981317426dde79a67d8fec176ace194b39390dc
Alexander <al.vi.sapelkin@gmail.com>
2018-09-20 17:40:57 +0300

    dsl improvement

--- bankinfo/src/test/kotlin/ru/alfabank/bankinfo/dsl/BankInfoDslBuilder.kt
+++ bankinfo/src/test/kotlin/ru/alfabank/bankinfo/dsl/BankInfoDslBuilder.kt
@@ -6,12 +6,11 @@ import ru.alfabank.bankinfo.model.BankBlzInfo
 class BankInfoDslBuilder{
     fun BlzInfoResponse(block: BankBlzInfo.() -> Unit): String {
         val bankInfo = BankBlzInfo().apply(block)
-        //В этой либе можно указывать xml неймспейсы, но я не сильно запаривался с этим
-        return xml("soapenv:Envelope") {
-            xmlns = "http://schemas.xmlsoap.org/soap/envelope/"
+        return xml("soapenv:Envelope",prettyFormat = false) {
+            namespace("soapenv","http://schemas.xmlsoap.org/soap/envelope/")
             "soapenv:Body"{
                 "ns1:getBankResponse"{
-                    attribute("xmlns:ns1","http://thomas-bayer.com/blz/")
+                    namespace("ns1","http://thomas-bayer.com/blz/")
                     "ns1:details"{
                         "ns1:bezeichnung"{
                             -bankInfo.bezeichnung
@@ -28,9 +27,7 @@ class BankInfoDslBuilder{
 
             }
         }.toString()
-                .replaceFirst("xmlns","xmlns:soapenv")
-                .replace("\t","")
-                .replace("\n","")
+
 
     }
 }
